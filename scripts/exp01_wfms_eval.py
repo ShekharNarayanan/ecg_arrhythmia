2c9d9b787f142552ce0eaa7e0bc44fa906ee77e3
ShekharNarayanan <shekharnarayanan833@gmail.com>
2026-02-24 17:33:24 +0100

    Add segment for class imbalance plot

--- scripts/exp01_wfms_eval.py
+++ scripts/exp01_wfms_eval.py
@@ -2,6 +2,7 @@ import joblib
 import matplotlib.pyplot as plt
 from sklearn.metrics import ConfusionMatrixDisplay, classification_report, f1_score
 from arrhythmia_ml import file_utils
+import numpy as np
 
 # load config params
 config = file_utils.load_config()
@@ -27,13 +28,46 @@ labels = ["N", "A", "V"]
 
 macro_f1 = f1_score(y_test, y_pred, average="macro")
 
-ConfusionMatrixDisplay.from_predictions(
-    y_test,
-    y_pred,
-    labels=labels,
-    display_labels=labels,
-    normalize="true",
-)
+# ConfusionMatrixDisplay.from_predictions(
+#     y_test,
+#     y_pred,
+#     labels=labels,
+#     display_labels=labels,
+#     normalize="true",
+# )
 
-plt.title(f"Patient-wise Confusion Matrix | Macro F1 = {macro_f1:.3f}")
+# plt.title(f"Patient-wise Confusion Matrix | Macro F1 = {macro_f1:.3f}")
+# plt.show()
+
+# show class imbalance
+
+y_train = data["y_train"]
+
+labels, counts = np.unique(y_train, return_counts=True)
+perc = (counts / counts.sum()) * 100
+
+plt.figure(figsize=(7, 5))
+
+bars = plt.bar(labels, perc, width=0.4, color = "#E65C00")
+
+plt.title("Class Distribution (Training Set)", fontsize=12, fontweight="bold")
+plt.xlabel("Class",fontweight="bold")
+plt.ylabel("Percentage (%)",fontweight="bold")
+
+plt.ylim(0, max(perc) * 1.15)  # add headroom
+
+# annotate bars
+for bar, p in zip(bars, perc):
+    plt.text(
+        bar.get_x() + bar.get_width() / 2,
+        bar.get_height(),
+        f"{p:.1f}%",
+        ha="center",
+        va="bottom",
+        fontsize=10,
+        fontweight="bold"
+    )
+plt.xticks(fontweight="bold")
+
+plt.tight_layout()
 plt.show()
